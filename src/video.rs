490ca667bdada6260ae226f82265f5a88fb962c9
Sean Buckley <sean.bck@gmail.com>
2025-04-08 00:22:40 -0400

    implement sprite transparency

--- src/video.rs
+++ src/video.rs
@@ -82,7 +82,7 @@ pub fn render_dot(gb: &mut GB, lx: u64, sprites: &Vec<Sprite>) {
 
 	let vbank = gb.bus.vram[gb.bus.io.vbk as usize];
 
-	if window_enable && wy <= lcd_y && wx <= lcd_x + 7 {
+	let map_pallete_index = if window_enable && wy <= lcd_y && wx <= lcd_x + 7 {
 		let win_y = lcd_y - wy;
 		let win_x = lcd_x + 7 - wx;
 
@@ -103,12 +103,15 @@ pub fn render_dot(gb: &mut GB, lx: u64, sprites: &Vec<Sprite>) {
 		let tile_y = win_y & 0b111;
 		let b1 = (tile_data[tile_y * 2 + 0] >> (7 - tile_x)) & 1;
 		let b2 = (tile_data[tile_y * 2 + 1] >> (7 - tile_x)) & 1;
+		let pallete_index = b1 | (b2 << 1);
 
-		let (r, g, b) = color_dmg(b1 | (b2 << 1), gb.bus.io.bgp);
+		let (r, g, b) = color_dmg(pallete_index, gb.bus.io.bgp);
 
 		gb.framebuffer[0 + 3 * (lcd_x + 160 * lcd_y)] = r;
 		gb.framebuffer[1 + 3 * (lcd_x + 160 * lcd_y)] = g;
 		gb.framebuffer[2 + 3 * (lcd_x + 160 * lcd_y)] = b;
+
+		pallete_index
 	} else {
 		let bg_x = 0xff & ((gb.bus.io.scx as usize) + lcd_x);
 		let bg_y = 0xff & ((gb.bus.io.scy as usize) + lcd_y);
@@ -129,14 +132,17 @@ pub fn render_dot(gb: &mut GB, lx: u64, sprites: &Vec<Sprite>) {
 		let tile_y = bg_y & 0b111;
 		let b1 = (tile_data[tile_y * 2 + 0] >> (7 - tile_x)) & 1;
 		let b2 = (tile_data[tile_y * 2 + 1] >> (7 - tile_x)) & 1;
+		let pallete_index = b1 | (b2 << 1);
 
 		// TODO: BG transparency
 
-		let (r, g, b) = color_dmg(b1 | (b2 << 1), gb.bus.io.bgp);
+		let (r, g, b) = color_dmg(pallete_index, gb.bus.io.bgp);
 		gb.framebuffer[0 + 3 * (lcd_x + 160 * lcd_y)] = r;
 		gb.framebuffer[1 + 3 * (lcd_x + 160 * lcd_y)] = g;
 		gb.framebuffer[2 + 3 * (lcd_x + 160 * lcd_y)] = b;
-	}
+
+		pallete_index
+	};
 	let sprite_h = match gb.bus.io.lcdc & 0b100 {
 		0 => 8,
 		_ => 16,
@@ -157,8 +163,15 @@ pub fn render_dot(gb: &mut GB, lx: u64, sprites: &Vec<Sprite>) {
 
 			let b1 = (tile_data[s_y * 2 + 0] >> (7 - s_x)) & 1;
 			let b2 = (tile_data[s_y * 2 + 1] >> (7 - s_x)) & 1;
+			let pallete_index = b1 | (b2 << 1);
 
+			// sprite is transparent here
 			if b1 | b2 == 0 {
+				continue;
+			}
+
+			// sprite's (low-)priority flag is set, and background/window is non-zero
+			if sprite.prio && map_pallete_index != 0 {
 				continue;
 			}
 
@@ -167,7 +180,7 @@ pub fn render_dot(gb: &mut GB, lx: u64, sprites: &Vec<Sprite>) {
 				true => gb.bus.io.obp1,
 			};
 
-			let (r, g, b) = color_dmg(b1 | (b2 << 1), palette);
+			let (r, g, b) = color_dmg(pallete_index, palette);
 
 			gb.framebuffer[0 + 3 * (lcd_x + 160 * lcd_y)] = r;
 			gb.framebuffer[1 + 3 * (lcd_x + 160 * lcd_y)] = g;
