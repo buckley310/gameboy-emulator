463528e9ff3162a3656bb27e35a6f42270aca33e
Sean Buckley <sean.bck@gmail.com>
2025-07-05 21:26:55 -0400

    define audio parameters better

--- src/audio.rs
+++ src/audio.rs
@@ -1,18 +1,85 @@
-use crate::ioreg::AUDIO_PARAMS_SIZE;
 use sdl2::audio::{AudioDevice, AudioSpecDesired};
 use std::sync::{Arc, Mutex};
 
-static AUDIO_FREQ: i32 = 48_000;
+const AUDIO_FREQ: i32 = 48_000;
+
+#[derive(Default)]
+struct Channel {
+	sweep: u8,
+	length: u8,
+	volume: u8,
+	frequency: u8,
+	control: u8,
+	trigger: bool,
+}
+
+#[derive(Default)]
+pub struct AudioParams {
+	channels: [Channel; 4],
+	nr50: u8,
+	nr51: u8,
+	nr52: u8,
+	wave_ram: [u8; 16],
+}
+impl AudioParams {
+	pub fn set(&mut self, addr: usize, data: u8) {
+		match addr {
+			0xFF10 => self.channels[0].sweep = data,
+			0xFF11 => self.channels[0].length = data,
+			0xFF12 => self.channels[0].volume = data,
+			0xFF13 => self.channels[0].frequency = data,
+			0xFF14 => {
+				self.channels[0].control = data;
+				if data & (1 << 7) != 0 {
+					self.channels[0].trigger = true;
+				}
+			}
+			0xFF15 => self.channels[1].sweep = data,
+			0xFF16 => self.channels[1].length = data,
+			0xFF17 => self.channels[1].volume = data,
+			0xFF18 => self.channels[1].frequency = data,
+			0xFF19 => {
+				self.channels[1].control = data;
+				if data & (1 << 7) != 0 {
+					self.channels[1].trigger = true;
+				}
+			}
+			0xFF1A => self.channels[2].sweep = data,
+			0xFF1B => self.channels[2].length = data,
+			0xFF1C => self.channels[2].volume = data,
+			0xFF1D => self.channels[2].frequency = data,
+			0xFF1E => {
+				self.channels[2].control = data;
+				if data & (1 << 7) != 0 {
+					self.channels[2].trigger = true;
+				}
+			}
+			0xFF1F => self.channels[3].sweep = data,
+			0xFF20 => self.channels[3].length = data,
+			0xFF21 => self.channels[3].volume = data,
+			0xFF22 => self.channels[3].frequency = data,
+			0xFF23 => {
+				self.channels[3].control = data;
+				if data & (1 << 7) != 0 {
+					self.channels[3].trigger = true;
+				}
+			}
+			0xFF24 => self.nr50 = data,
+			0xFF25 => self.nr51 = data,
+			0xFF26 => self.nr52 = data,
+			0xFF30..=0xFF3F => self.wave_ram[addr - 0xFF30] = data,
+			_ => println!("invalid audio write! {addr:#x}"),
+		}
+	}
+}
 
 pub struct PcmGenerator {
-	callback_i: u64,
 	phase: i32,
-	audio_params: Arc<Mutex<[(u8, bool); AUDIO_PARAMS_SIZE]>>,
+	audio_params: Arc<Mutex<AudioParams>>,
 }
 impl PcmGenerator {
-	fn new(audio_params: Arc<Mutex<[(u8, bool); AUDIO_PARAMS_SIZE]>>) -> Self {
+	fn new(audio_params: Arc<Mutex<AudioParams>>) -> Self {
 		PcmGenerator {
-			callback_i: 0,
 			phase: 0,
 			audio_params,
 		}
@@ -21,33 +88,30 @@ impl PcmGenerator {
 impl sdl2::audio::AudioCallback for PcmGenerator {
 	type Channel = i32;
 	fn callback(&mut self, out: &mut [i32]) {
-		println!("snd {} {}", out.len(), self.callback_i);
-		self.callback_i += 1;
-
 		// very incomplete, only reads channel 2
 		let audio_params = self.audio_params.lock().unwrap();
-		let vol = (audio_params[0x7].0 as i32) << 18;
+		let vol = (audio_params.channels[1].volume as i32) << 18;
 		for x in out.iter_mut() {
 			*x = if self.phase < 0x800 { vol } else { -vol };
-			self.phase += audio_params[0x8].0 as i32;
+			self.phase += audio_params.channels[1].frequency as i32;
 			self.phase &= 0xfff;
 		}
 	}
 }
 
 pub struct APU {
-	pub audio_params: Arc<Mutex<[(u8, bool); AUDIO_PARAMS_SIZE]>>,
+	pub audio_params: Arc<Mutex<AudioParams>>,
 	pub device: AudioDevice<PcmGenerator>,
 }
 impl std::default::Default for APU {
 	fn default() -> Self {
-		let audio_params = Arc::new(Mutex::new([(0, false); AUDIO_PARAMS_SIZE]));
+		let audio_params = Arc::new(Mutex::new(AudioParams::default()));
 		let sdl_context = sdl2::init().unwrap();
 		let audio_subsystem = sdl_context.audio().unwrap();
 		let desired_spec = AudioSpecDesired {
 			freq: Some(AUDIO_FREQ),
 			channels: Some(1),
-			samples: None, // TODO: manual sample count?
+			samples: Some((AUDIO_FREQ / 100) as u16),
 		};
 		let device = audio_subsystem
 			.open_playback(None, &desired_spec, |_| {
