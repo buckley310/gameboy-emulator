69aa90ba295efb23bbf9b8294802c1e89e710781
Sean Buckley <sean.bck@gmail.com>
2025-10-18 02:20:48 -0400

    move audio locking out of channel2

--- src/audio.rs
+++ src/audio.rs
@@ -2,7 +2,7 @@ use raylib::prelude::*;
 use std::sync::{Arc, Mutex};
 
 const AUDIO_FREQ: u16 = 48_000;
-const MASTER_VOLUME: f32 = 0.2;
+const VOLUME_DIAL: u8 = 50;
 
 #[derive(Default)]
 struct Channel {
@@ -75,8 +75,6 @@ impl AudioParams {
 }
 
 pub struct PcmGenerator {
-	audio_params: Arc<Mutex<AudioParams>>,
-
 	c2_freq: usize,
 	c2_freq_low: usize,
 	c2_phase: usize,
@@ -86,10 +84,8 @@ pub struct PcmGenerator {
 	c2_env_direction: bool,
 }
 impl PcmGenerator {
-	fn new(audio_params: Arc<Mutex<AudioParams>>) -> Self {
+	fn new() -> Self {
 		PcmGenerator {
-			audio_params,
-
 			c2_freq: 0,
 			c2_freq_low: 0,
 			c2_phase: 0,
@@ -99,49 +95,45 @@ impl PcmGenerator {
 			c2_env_direction: false,
 		}
 	}
-	fn callback(&mut self, buf: &mut [u8]) {
+	fn trigger(&mut self, channel: &mut Channel) {
 		fn from_ticks(tick_hz: usize, n: usize) -> usize {
 			(AUDIO_FREQ as usize) * n / tick_hz
 		}
-		let mut audio_params = self.audio_params.lock().unwrap();
-		if audio_params.channels[1].trigger {
-			audio_params.channels[1].trigger = false;
-			self.c2_env_high = from_ticks(64, audio_params.channels[1].volume as usize & 7);
-			self.c2_env = self.c2_env_high;
-			self.c2_env_direction = audio_params.channels[1].volume & 8 != 0;
-			self.c2_volume = audio_params.channels[1].volume & 0xF0;
-			let period = (audio_params.channels[1].frequency as usize)
-				| ((audio_params.channels[1].control as usize & 0b111) << 8);
-			self.c2_freq = from_ticks(1 << 17, (1 << 11) - period);
-			self.c2_freq_low = match audio_params.channels[1].length >> 6 {
-				0 => self.c2_freq >> 3,
-				1 => self.c2_freq >> 2,
-				2 => self.c2_freq >> 1,
-				3 => (self.c2_freq >> 2) * 3,
-				_ => panic!(),
-			};
+		channel.trigger = false;
+		self.c2_env_high = from_ticks(64, channel.volume as usize & 7);
+		self.c2_env = self.c2_env_high;
+		self.c2_env_direction = channel.volume & 8 != 0;
+		self.c2_volume = channel.volume & 0xF0;
+		let period = (channel.frequency as usize) | ((channel.control as usize & 0b111) << 8);
+		self.c2_freq = from_ticks(1 << 17, (1 << 11) - period);
+		self.c2_freq_low = match channel.length >> 6 {
+			0 => self.c2_freq >> 3,
+			1 => self.c2_freq >> 2,
+			2 => self.c2_freq >> 1,
+			3 => (self.c2_freq >> 2) * 3,
+			_ => panic!(),
+		};
+	}
+	fn callback(&mut self) -> f32 {
+		if self.c2_env_high != 0 {
+			self.c2_env = self.c2_env.saturating_sub(1);
+			if self.c2_env == 0 {
+				self.c2_env = self.c2_env_high;
+				self.c2_volume = match self.c2_env_direction {
+					true => self.c2_volume.saturating_add(0x10),
+					false => self.c2_volume.saturating_sub(0x10),
+				};
+			}
 		}
-		for out in buf.iter_mut() {
-			if self.c2_env_high != 0 {
-				self.c2_env = self.c2_env.saturating_sub(1);
-				if self.c2_env == 0 {
-					self.c2_env = self.c2_env_high;
-					self.c2_volume = match self.c2_env_direction {
-						true => self.c2_volume.saturating_add(0x10),
-						false => self.c2_volume.saturating_sub(0x10),
-					};
-				}
-			}
-			self.c2_phase = self.c2_phase.saturating_sub(1);
-			if self.c2_phase == 0 {
-				self.c2_phase = self.c2_freq;
-			}
-			let magnitude = (self.c2_volume as f32) / 256.0 * MASTER_VOLUME;
-			*out = if self.c2_phase < self.c2_freq_low {
-				128 - (magnitude * 127.0) as u8
-			} else {
-				128 + (magnitude * 127.0) as u8
-			};
+		self.c2_phase = self.c2_phase.saturating_sub(1);
+		if self.c2_phase == 0 {
+			self.c2_phase = self.c2_freq;
+		}
+		let magnitude = self.c2_volume as f32 / 256.0;
+		if self.c2_phase < self.c2_freq_low {
+			-magnitude
+		} else {
+			magnitude
 		}
 	}
 }
@@ -162,10 +154,19 @@ impl<'a> APU<'a> {
 		let audio_params = Arc::new(Mutex::new(AudioParams::default()));
 		let stream = device.new_audio_stream(AUDIO_FREQ as u32, 8, 1);
 
-		let mut pcm_generator = PcmGenerator::new(audio_params.clone());
+		let mut pcm_generator = PcmGenerator::new();
 
+		let audio_params_for_callback = audio_params.clone();
 		audio_stream_callback::set_audio_stream_callback(&stream, move |buf: &mut [u8]| {
-			pcm_generator.callback(buf);
+			let mut p = audio_params_for_callback.lock().unwrap();
+			if p.channels[1].trigger {
+				pcm_generator.trigger(&mut p.channels[1]);
+			}
+			for out in buf.iter_mut() {
+				let c1 = pcm_generator.callback();
+				assert!(c1 >= -1.0 && c1 <= 1.0);
+				*out = ((c1 / 2.0 + 0.5) * (VOLUME_DIAL as f32)) as u8;
+			}
 		})
 		.expect("error activating audio callback");
 
